ee277a71df4e6112f384e84cee357a77229c6521
Vinor <vinorval@yandex.ru>
2023-08-09 10:12:00 +0300

    remove duplicate lines mainApi

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -3,6 +3,14 @@ class MainApi {
       this._url = config.url;
       this.headers = config.headers;
     }
+
+    _createRequest(url, method, headers, body) {
+      return fetch(url, {
+        method: method,
+        headers: headers,
+        body: body
+      })
+    }
   
     _checkResponse(res) {
       if (res.ok) {
@@ -13,50 +21,35 @@ class MainApi {
   
     //получение информации о пользователе с сервера
     getProfileInfo(token) {
-      return fetch(`${this._url}/users/me`, {
-        method: "GET",
-        headers: {
-          ...this.headers,
-          Authorization: `Bearer ${token}`,
-        },
-      }).then(this._checkResponse);
+      return this._createRequest(`${this._url}/users/me`, "GET", { ...this.headers, Authorization: `Bearer ${token}` }).then(this._checkResponse);
     }
   
     //передача новой информации о пользователе на сервер
     putProfileInfo(data, token) {
-      return fetch(`${this._url}/users/me`, {
-        method: "PATCH",
-        headers: {
-          ...this.headers,
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          name: data.name,
-          email: data.email,
-        }),
-      }).then(this._checkResponse);
+      return this._createRequest(
+        `${this._url}/users/me`,
+        "PATCH",
+        { ...this.headers, Authorization: `Bearer ${token}` },
+        JSON.stringify({ name: data.name, email: data.email,})
+      ).then(this._checkResponse);
     }
 
     //получение карточек с сервера
   getAllCard(token) {
-    return fetch(`${this._url}/movies`, {
-      method: "GET",
-      headers: {
-        ...this.headers,
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(this._checkResponse);
+    return this._createRequest(
+      `${this._url}/movies`,
+      "GET",
+      { ...this.headers, Authorization: `Bearer ${token}` },
+    ).then(this._checkResponse);
   }
 
   //добавление карточки на сервер
   addCard(data, token) {
-    return fetch(`${this._url}/movies`, {
-      method: "POST",
-      headers: {
-        ...this.headers,
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
+    return this._createRequest(
+      `${this._url}/movies`,
+      "POST",
+      { ...this.headers, Authorization: `Bearer ${token}` },
+      JSON.stringify({ 
         country: data.country,
         director: data.director,
         duration: data.duration,
@@ -68,19 +61,17 @@ class MainApi {
         movieId: data.id,
         nameRU: data.nameRU,
         nameEN: data.nameEN,
-      }),
-    }).then(this._checkResponse);
+      })
+    ).then(this._checkResponse);
   }
 
   //удаление карточки с сервера
   removeCard(cardId, token) {
-    return fetch(`${this._url}/movies/${cardId}`, {
-      method: "DELETE",
-      headers: {
-        ...this.headers,
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(this._checkResponse);
+    return this._createRequest(
+      `${this._url}/movies/${cardId}`,
+      "DELETE",
+      { ...this.headers, Authorization: `Bearer ${token}` },
+    ).then(this._checkResponse);
   }
   }
   
